8df84eb78d201ab82c19b7cbb8dedf28a87832b7
ApoDarkMatter <ndrpln@gmail.com>
2023-07-12 21:11:06 +0200

    add arrow function to navigate

---
+++ Corso/M3/D7-D8/script/script.js
@@ -0,0 +1,8 @@
+function leftScroll(idCard) {
+    const left = document.getElementById(idCard);
+    left.scrollBy(-200, 0);
+}
+function rightScroll(idCard) {
+    const right = document.getElementById(idCard);
+    right.scrollBy(200, 0);
+}
